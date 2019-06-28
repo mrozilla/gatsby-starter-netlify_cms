7d9a238e73fe50342a0cae76609afe2d5dd6bb82
jan <jan@mrozilla.cz>
2019-06-28 20:20:41 +0200

    update footer styles logic

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -10,8 +10,10 @@ import { View } from '~components/primitives/View';
 // component
 // ─────────────────────────────────────────────────────────────────────────────
 
-export const Footer = styled(View)`
-  background-color: var(--color-inverse);
-  box-shadow: inset 0 1px hsla(var(--hsl-text), 0.1);
-`;
-Footer.defaultProps = { as: 'footer', display: 'grid' };
+export const Footer = styled(View)``;
+Footer.defaultProps = {
+  as:              'footer',
+  display:         'grid',
+  backgroundColor: 'var(--color-inverse)',
+  boxShadow:       'inset 0 1px hsla(var(--hsl-text), 0.1)',
+};
