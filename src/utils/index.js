8f48e112d1ad3b68cada2ddc82767bbbaae1a062
jan <jan@mrozilla.cz>
2019-07-02 15:34:56 +0200

    add hooks

--- src/utils/index.js
+++ src/utils/index.js
@@ -4,3 +4,5 @@ export * from './style/animations';
 
 export { default as useInactiveTab } from './hooks/useInactiveTab';
 export { default as useLocalStorage } from './hooks/useLocalStorage';
+export { default as useSocialShare } from './hooks/useSocialShare';
+export { default as useSelection } from './hooks/useSelection';
